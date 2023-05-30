c6f8cac1e461dc36df6b53e269fe1b1cc02c68b3
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-30 20:29:14 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})

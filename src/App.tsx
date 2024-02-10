7371a1ae620e2e1377f6fe8d28e23d4d3e7a44e2
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-02-10 13:30:50 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button as={'a'} href={'/link'} variant={'primary'}>
+        Hello
+      </Button>
+      <Button variant={'primary'}>Hello</Button>
+    </div>
+  )
 }

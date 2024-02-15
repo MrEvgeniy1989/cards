e2bcfa13aaa8ae368037102a9fbd1851f5e4639c
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-02-15 21:19:10 +0300

    refactor: Made styles for pagination.

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { CSSProperties, useState } from 'react'
 
 import { ArrowDown } from '@/assets/icons/arrowDown'
 import { ArrowUp } from '@/assets/icons/arrowUp'
@@ -11,40 +11,69 @@ type Props = {
   disabled?: boolean
   items: string[]
   label?: string
+  styleForContent?: CSSProperties
+  styleForItem?: CSSProperties
+  styleForTrigger?: CSSProperties
 }
 
-export const MySelect = ({ disabled, items, label }: Props) => {
+export const MySelect = ({
+  disabled,
+  items,
+  label,
+  styleForContent,
+  styleForItem,
+  styleForTrigger,
+}: Props) => {
   const [open, setOpen] = useState(false)
 
   return (
     <>
       <SelectRadix.Root onOpenChange={() => setOpen(!open)}>
         <div className={s.selectRoot}>
-          <Typography
-            as={'label'}
-            className={disabled ? s.labelDisabled : s.label}
-            variant={'body2'}
-          >
-            {label}
-          </Typography>
+          {label && (
+            <Typography
+              as={'label'}
+              className={disabled ? s.labelDisabled : s.label}
+              variant={'body2'}
+            >
+              {label}
+            </Typography>
+          )}
 
-          <SelectRadix.Trigger className={s.SelectTrigger} disabled={disabled}>
-            <SelectRadix.Value defaultValue={items[0]} placeholder={items[0]} />
+          <SelectRadix.Trigger
+            className={s.SelectTrigger}
+            disabled={disabled}
+            style={styleForTrigger}
+          >
+            <SelectRadix.Value
+              className={s.SelectValue}
+              defaultValue={items[0]}
+              placeholder={items[0]}
+            />
             {open ? <ArrowUp className={s.SelectIcon} /> : <ArrowDown className={s.SelectIcon} />}
           </SelectRadix.Trigger>
-        </div>
 
-        <SelectRadix.Content className={s.SelectContent} position={'popper'}>
-          <SelectRadix.Viewport className={s.SelectViewport}>
-            {items.map(item => {
-              return (
-                <SelectRadix.Item className={s.SelectItem} key={item} value={item}>
-                  <SelectRadix.ItemText>{item}</SelectRadix.ItemText>
-                </SelectRadix.Item>
-              )
-            })}
-          </SelectRadix.Viewport>
-        </SelectRadix.Content>
+          <SelectRadix.Content
+            className={s.SelectContent}
+            position={'popper'}
+            style={styleForContent}
+          >
+            <SelectRadix.Viewport className={s.SelectViewport}>
+              {items.map(item => {
+                return (
+                  <SelectRadix.Item
+                    className={s.SelectItem}
+                    key={item}
+                    style={styleForItem}
+                    value={item}
+                  >
+                    <SelectRadix.ItemText>{item}</SelectRadix.ItemText>
+                  </SelectRadix.Item>
+                )
+              })}
+            </SelectRadix.Viewport>
+          </SelectRadix.Content>
+        </div>
       </SelectRadix.Root>
     </>
   )

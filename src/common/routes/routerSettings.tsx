37158b377d19e14679776feaae8444e246b3660a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-02 22:03:53 +0300

    refactor: Replaced a regular TextField with a ControlledTextField. Created ForgotPasswordPage and configured routing for it.

--- src/common/routes/routerSettings.tsx
+++ src/common/routes/routerSettings.tsx
@@ -2,6 +2,7 @@ import { Navigate, RouteObject } from 'react-router-dom'
 
 import { CreateNewPasswordPage } from '@/pages/createNewPasswordPage'
 import { DecksPage } from '@/pages/decks/DecksPage'
+import { ForgotPasswordPage } from '@/pages/forgonPassword'
 import { SignInPage } from '@/pages/signIn'
 import { SignUpPage } from '@/pages/signUpPage'
 
@@ -13,4 +14,5 @@ export const publicRoutes: RouteObject[] = [
   { element: <SignInPage />, path: '/login' },
   { element: <SignUpPage />, path: '/sign-up' },
   { element: <CreateNewPasswordPage />, path: '/create-new-password/:token' },
+  { element: <ForgotPasswordPage />, path: '/forgot-password' },
 ]
---
+++ src/pages/forgonPassword/ForgotPasswordPage.tsx
@@ -0,0 +1,10 @@
+import { Page } from '@/common/components/ui/page'
+import { ForgotPassword } from '@/feature/auth/ui/forgotPassword'
+
+export const ForgotPasswordPage = () => {
+  return (
+    <Page>
+      <ForgotPassword onSubmit={() => {}} />
+    </Page>
+  )
+}

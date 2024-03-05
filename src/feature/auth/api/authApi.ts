79e047033012b21ffdd7e13150b28537d70ab71d
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-05 19:30:37 +0300

    feat: Made an authApi and types for it.

---
+++ src/feature/auth/api/authApi.ts
@@ -0,0 +1,76 @@
+import { baseApi } from '@/common/api/baseApi'
+import {
+  LoginParams,
+  LoginResponse,
+  RecoverPasswordParams,
+  ResetPasswordParams,
+  SignUpParams,
+  User,
+} from '@/feature/auth/api/auth.types'
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation<LoginResponse, LoginParams>({
+      invalidatesTags: ['Me'],
+      query: body => ({
+        body,
+        method: 'POST',
+        url: 'v1/auth/login',
+      }),
+    }),
+    logout: builder.mutation<void, void>({
+      invalidatesTags: ['Me'],
+      query: () => ({
+        method: 'POST',
+        url: 'v1/auth/logout',
+      }),
+    }),
+    me: builder.query<User | null, void>({
+      providesTags: ['Me'],
+      query: () => 'v1/auth/me',
+    }),
+    recoverPassword: builder.mutation<void, RecoverPasswordParams>({
+      query: body => ({
+        body: {
+          email: body.email,
+          html: '<h1>Hi, ##name##</h1><p>Click <a href="https://flashcards-learning.vercel.app/create-new-password/##token##">here</a> to recover your password</p>',
+          subject: 'Recovery Password',
+        },
+        method: 'POST',
+        url: 'v1/auth/recover-password',
+      }),
+    }),
+    resetPassword: builder.mutation<void, ResetPasswordParams>({
+      query: ({ password, token }) => ({
+        body: { password },
+        method: 'POST',
+        url: `v1/auth/reset-password/${token}`,
+      }),
+    }),
+    signUp: builder.mutation<User, SignUpParams>({
+      invalidatesTags: ['Me'],
+      query: body => ({
+        body,
+        method: 'POST',
+        url: 'v1/auth/sign-up',
+      }),
+    }),
+    updateProfile: builder.mutation<User, FormData>({
+      query: body => ({
+        body,
+        method: 'PATCH',
+        url: 'v1/auth/me',
+      }),
+    }),
+  }),
+})
+
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useMeQuery,
+  useRecoverPasswordMutation,
+  useResetPasswordMutation,
+  useSignUpMutation,
+  useUpdateProfileMutation,
+} = authApi

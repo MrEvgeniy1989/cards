548aa31487bf6821df09e7dc8bf7a4b7e1678932
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-05 16:53:40 +0300

    feat: Made decksApi.ts and types for it.

---
+++ src/feature/decks/api/decksApi.ts
@@ -0,0 +1,65 @@
+import { baseApi } from '@/common/api/baseApi'
+import {
+  Deck,
+  DeckWithAuthor,
+  GetDecksParamsType,
+  MinMaxCardsResponse,
+  UpdateDeckParamsType,
+  getDecksResponse,
+} from '@/feature/decks/api/decksApi.types'
+
+export const decksApi = baseApi.injectEndpoints({
+  endpoints: builder => ({
+    createDeck: builder.mutation<DeckWithAuthor, void>({
+      invalidatesTags: ['Decks'],
+      query: body => ({
+        body,
+        method: 'POST',
+        url: 'v1/decks',
+      }),
+    }),
+    deleteDeck: builder.mutation<Deck, { id: string }>({
+      query: ({ id }) => ({
+        method: 'DELETE',
+        url: `v1/decks/${id}}`,
+      }),
+    }),
+    getDeckById: builder.query<DeckWithAuthor, { id: string }>({
+      providesTags: ['Decks', { id: 'List', type: 'Decks' }],
+      query: ({ id }) => ({
+        method: 'GET',
+        url: `v1/decks/${id}`,
+      }),
+    }),
+    getDecks: builder.query<getDecksResponse, GetDecksParamsType | void>({
+      providesTags: ['Decks'],
+      query: params => ({
+        method: 'GET',
+        params: params ?? {},
+        url: 'v2/decks',
+      }),
+    }),
+    getMinMaxCards: builder.query<MinMaxCardsResponse, void>({
+      query: () => ({
+        method: 'GET',
+        url: 'v2/decks/min-max-cards',
+      }),
+    }),
+    updateDeck: builder.mutation<DeckWithAuthor, UpdateDeckParamsType>({
+      query: ({ body, id }) => ({
+        body,
+        method: 'PATCH',
+        url: `v1/decks/${id}}`,
+      }),
+    }),
+  }),
+})
+
+export const {
+  useCreateDeckMutation,
+  useDeleteDeckMutation,
+  useGetDeckByIdQuery,
+  useGetDecksQuery,
+  useGetMinMaxCardsQuery,
+  useUpdateDeckMutation,
+} = decksApi

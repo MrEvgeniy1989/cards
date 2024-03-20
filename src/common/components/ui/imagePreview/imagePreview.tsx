884b00bda18ae3dd978c92f4c6a312d7f3366499
Vasilievkosta <vasilievkosta@gmail.com>
2024-03-20 13:44:10 +0200

    feat: add imagePreview

---
+++ src/common/components/ui/imagePreview/imagePreview.tsx
@@ -0,0 +1,31 @@
+import { Dialog, DialogContent, DialogOverlay, DialogPortal } from '@radix-ui/react-dialog'
+
+import s from './imagePreview.module.scss'
+
+export type ImagePreviewProps = {
+  alt?: string
+  onClose?: () => void
+  open: boolean
+  src: string
+}
+export const ImagePreview = ({ alt, onClose, open, src }: ImagePreviewProps) => {
+  const handleModalClosed = () => {
+    onClose?.()
+  }
+
+  return (
+    <Dialog onOpenChange={handleModalClosed} open={open}>
+      <DialogPortal>
+        <DialogOverlay className={s.overlay} />
+        <DialogContent asChild className={s.content}>
+          <img
+            alt={alt ?? 'preview'}
+            className={s.imageContent}
+            onClick={handleModalClosed}
+            src={src}
+          />
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  )
+}

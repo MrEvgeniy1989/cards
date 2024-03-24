121cb280d10129f83383b206adb922ea62bba139
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-24 22:28:33 +0300

    feat: Wrapped the story decorator in forgotPassword.stories.tsx

--- src/feature/auth/ui/forgotPassword/forgotPassword.stories.tsx
+++ src/feature/auth/ui/forgotPassword/forgotPassword.stories.tsx
@@ -1,9 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { BrowserRouter } from 'react-router-dom'
+
 import { ForgotPassword } from '@/feature/auth/ui/forgotPassword/ForgotPassword'
 
 const meta = {
   component: ForgotPassword,
+  decorators: [
+    Story => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
   parameters: {
     layout: 'centered',
   },

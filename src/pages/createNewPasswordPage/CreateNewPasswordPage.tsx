31e187d529ff3acb660109d544a53b4fce1eccf1
Vasilievkosta <vasilievkosta@gmail.com>
2024-03-28 10:56:53 +0200

    chore: add confirm password

--- src/pages/createNewPasswordPage/CreateNewPasswordPage.tsx
+++ src/pages/createNewPasswordPage/CreateNewPasswordPage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
 
 import { Page } from '@/common/components/ui/page'
 import { useResetPasswordMutation } from '@/feature/auth/api/authApi'
@@ -21,12 +22,13 @@ export const CreateNewPasswordPage = () => {
         .then(_ => {
           navigate('/login')
         })
-        .catch(e => console.error(JSON.stringify(e)))
+        .catch(e => toast.error(e?.data?.message ?? 'password has not been changed'))
   }
 
   return (
     <Page>
       <CreateNewPassword onSubmit={newPasswordHandler} />
+      <ToastContainer />
     </Page>
   )
 }

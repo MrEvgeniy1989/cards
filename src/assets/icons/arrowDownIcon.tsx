d59da3faf045127cbf68df0815b63c350bba14f1
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-30 23:11:00 +0300

    feat: Redesigned the ArrowDownIcon so that it changes when the theme changes.

--- src/assets/icons/arrowDownIcon.tsx
+++ src/assets/icons/arrowDownIcon.tsx
@@ -1,8 +1,8 @@
 import { SVGProps, memo } from 'react'
 
-export const ArrowDown = memo((props: SVGProps<SVGSVGElement>) => (
+export const ArrowDownIcon = memo((props: SVGProps<SVGSVGElement>) => (
   <svg
-    fill={props.fill ?? 'white'}
+    fill={'var(--color-light-100)'}
     height={props.height ?? 16}
     width={props.width ?? 16}
     xmlns={'http://www.w3.org/2000/svg'}
@@ -13,12 +13,12 @@ export const ArrowDown = memo((props: SVGProps<SVGSVGElement>) => (
         d={
           'M3.676 6.306a.667.667 0 0 1 1.093-.514L8.343 8.78l3.58-2.88a.666.666 0 0 1 .94.1.666.666 0 0 1-.1.973l-4 3.22a.666.666 0 0 1-.847 0l-4-3.333a.667.667 0 0 1-.24-.553Z'
         }
-        fill={props.fill}
+        fill={'var(--color-light-100)'}
       />
     </g>
     <defs>
       <clipPath id={'a'}>
-        <path d={'M0 0h16v16H0z'} fill={'#fff'} />
+        <path d={'M0 0h16v16H0z'} fill={'var(--color-light-100)'} />
       </clipPath>
     </defs>
   </svg>

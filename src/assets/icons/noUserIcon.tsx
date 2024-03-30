60f61c8a7fd5428af1e357c4775e6d514a1192bb
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-30 23:14:25 +0300

    feat: Redesigned the NoUserIcon so that it changes when the theme changes.

--- src/assets/icons/noUserIcon.tsx
+++ src/assets/icons/noUserIcon.tsx
@@ -1,5 +1,5 @@
-import { SVGProps } from 'react'
-const IconNoUserIcon = (props: SVGProps<SVGSVGElement>) => (
+import { SVGProps, memo } from 'react'
+export const NoUserIcon = memo((props: SVGProps<SVGSVGElement>) => (
   <svg
     height={36}
     viewBox={'0 0 389.34 389.34'}
@@ -13,7 +13,7 @@ const IconNoUserIcon = (props: SVGProps<SVGSVGElement>) => (
         'M347.415 330.3v.13c-40.37 36.61-93.96 58.91-152.74 58.91-58.79 0-112.38-22.3-152.75-58.91v-.13c0-60.89 35.63-113.45 87.17-137.98 19.37 19.32 42.05 29.73 65.58 29.73 23.54 0 46.21-10.41 65.59-29.73 51.54 24.53 87.15 77.09 87.15 137.98z'
       }
       style={{
-        fill: '#1ea6c6',
+        fill: 'var(--color-accent-500)',
       }}
     />
     <path
@@ -21,10 +21,8 @@ const IconNoUserIcon = (props: SVGProps<SVGSVGElement>) => (
         'M194.675 0c46.66 0 84.49 37.82 84.49 84.48 0 46.67-37.83 110.49-84.49 110.49s-84.49-63.82-84.49-110.49c0-46.66 37.83-84.48 84.49-84.48z'
       }
       style={{
-        fill: '#fcd09f',
+        fill: 'var(--color-user-head)',
       }}
     />
   </svg>
-)
-
-export default IconNoUserIcon
+))

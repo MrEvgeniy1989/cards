e800b268be08ae1f1f762f865d8c95f12be2be57
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-30 23:18:30 +0300

    refactor: Deleted some unused files.

--- src/assets/icons/edit-outline/typeIcons.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentPropsWithoutRef } from 'react'
-
-export type IconProps = {
-  color?: string | undefined
-  version?: ThemeApp
-} & ComponentPropsWithoutRef<'svg'>
-export type ThemeApp = 'dark' | 'light' | 'red'

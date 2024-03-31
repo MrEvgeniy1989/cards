50d5dc68b36de098409ce07e4c04ff340a979275
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-31 15:10:07 +0300

    refactor: Changed the CreateNewPasswordPage and CreateNewPassword components. We've added confirm password.

--- src/feature/auth/ui/createNewPassword/CreateNewPassword.tsx
+++ src/feature/auth/ui/createNewPassword/CreateNewPassword.tsx
@@ -9,9 +9,19 @@ import z from 'zod'
 
 import s from '@/feature/auth/ui/createNewPassword/createNewPassword.module.scss'
 
-const newPasswordSchema = z.object({
-  password: z.string().min(3),
-})
+const newPasswordSchema = z
+  .object({
+    password: z.string().min(3, {
+      message: 'your password is too short',
+    }),
+    repeatPassword: z.string().min(3, {
+      message: 'please repeat you password',
+    }),
+  })
+  .refine(data => data.password === data.repeatPassword, {
+    message: 'passwords do not match',
+    path: ['repeatPassword'],
+  })
 
 export type FormValues = z.infer<typeof newPasswordSchema>
 type Props = {
@@ -25,6 +35,7 @@ export const CreateNewPassword = ({ onSubmit }: Props) => {
   } = useForm<FormValues>({
     defaultValues: {
       password: '',
+      repeatPassword: '',
     },
     resolver: zodResolver(newPasswordSchema),
   })
@@ -41,9 +52,19 @@ export const CreateNewPassword = ({ onSubmit }: Props) => {
           error={errors.password?.message}
           label={'Password'}
           name={'password'}
-          placeholder={'Password'}
+          placeholder={'password'}
           type={'password'}
         />
+        <ControlledTextField
+          className={s.inputPassword}
+          control={control}
+          error={errors.repeatPassword?.message}
+          label={'Confirm password'}
+          name={'repeatPassword'}
+          placeholder={'repeat password'}
+          type={'password'}
+        />
+
         <Typography className={s.infoText} variant={'body2'}>
           Create new password and we will send you further instructions to email
         </Typography>

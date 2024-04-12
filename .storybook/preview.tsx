68c2bcf72f6cc058a123f4d55996af353a3c10b2
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-12 11:52:27 +0300

    feat: Reworked the theme toggle, instead of setting the class to body, I did it via attributes for :root (:root[data-theme='dark']). Made the theme display correctly in StoryBook.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,20 +2,29 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '@/app/styles/index.scss'
 import type { Preview } from '@storybook/react'
-import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider } from '../src/common/components/ui/themeProvider/ThemeProvider'
 
 const preview: Preview = {
   decorators: [
     Story => (
       <MemoryRouter>
-        <div className="dark-theme">
+        <ThemeProvider>
           <Story />
-        </div>
+        </ThemeProvider>
       </MemoryRouter>
     ),
   ],
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: 'var(--color-dark-900)',
+        },
+      ],
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
